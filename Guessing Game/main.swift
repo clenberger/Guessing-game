b37d36a833f22937e38b9151cf1a5f68d5bc5d28
Christian Lenberger <christianlenberger@Interapts-MacBook-Pro.local>
2018-01-23 15:44:54 -0500

    first commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Christian Lenberger on 1/23/18.
+//  Copyright © 2018 Interapt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

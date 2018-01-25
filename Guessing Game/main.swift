c5862388bf7fa837be828c08a2d5fc97c2ece30f
Christian Lenberger <christianlenberger@Interapts-MacBook-Pro.local>
2018-01-25 13:25:58 -0500

    Added user input logic

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+//Generate a random number
+var randomNumber = Int(arc4random_uniform(100))
+var guess: Int?
+
+
+//Ask user for input
+print("Please guess a number")
+//Take user input
+for _ in 0...4{
+guess = Int(readLine()!)!
+
+// They guessed correctly
+
+if randomNumber == guess {
+    print("YOU WON!!!!!!!")
+} else if randomNumber > guess! {
+    print("The number was higher")
+} else if randomNumber < guess! {
+print("The number was lower")
+    }
+
+}
+
+print("The actual number was \(randomNumber), sorry pal.")
+
+//Determine wrong or right (Loop until correct)
+
+//If correct ask if they want to play again
+
+//If incorrect tell them the correct answer and ask them to play again
 

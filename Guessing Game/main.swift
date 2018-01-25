cb534ace00e0a293cc47587e5fbca81c4dd17993
Christian Lenberger <christianlenberger@Interapts-MacBook-Pro.local>
2018-01-25 16:41:35 -0500

    Added loops

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -11,31 +11,42 @@ import Foundation
 //Generate a random number
 var randomNumber = Int(arc4random_uniform(100))
 var guess: Int?
+var tries = 1
+var playAgain = "Y"
 
+while playAgain == "Y"{
 
-//Ask user for input
-print("Please guess a number")
-//Take user input
-for _ in 0...4{
-guess = Int(readLine()!)!
+    print("Please guess a number between 1 and 100. You have 5 guesses.")
+    guess = Int(readLine()!)!
 
-// They guessed correctly
-
-if randomNumber == guess {
-    print("YOU WON!!!!!!!")
-} else if randomNumber > guess! {
+while guess! != randomNumber && tries < 5{
+if randomNumber > guess! {
     print("The number was higher")
-} else if randomNumber < guess! {
-print("The number was lower")
+}
+else if randomNumber < guess! {
+
+    print("The number was lower")
     }
 
+    print("Please guess again")
+
+    guess = Int(readLine()!)!
+
+    tries += 1
+    }
+if randomNumber == guess! {
+        print("YOU WON!!!!!!!")
+    }
+if randomNumber != guess!{
+print("The actual number was \(randomNumber)")
+    }
+
+    randomNumber = Int(arc4random_uniform(100))
+    tries = 1
+    print("Would you like to play again? Y/N")
+    playAgain = readLine()!
+    guess = Int(EMPTY)
+    
+
+
 }
-
-print("The actual number was \(randomNumber), sorry pal.")
-
-//Determine wrong or right (Loop until correct)
-
-//If correct ask if they want to play again
-
-//If incorrect tell them the correct answer and ask them to play again
-
